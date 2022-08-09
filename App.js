bf3ca8092235ded5a5b38dbb5b9a489af73259f9
Matheus Vilas <matheus.vilas@casion.com.br>
2022-08-09 10:47:13 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import OneSignal from "react-native-onesignal";
+import Constants from "expo-constants";
+OneSignal.setAppId(Constants.manifest.extra.oneSignalAppId);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          marginTop: 40,
+        }}
+      >
+        {Constants.manifest.extra.oneSignalAppId}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
